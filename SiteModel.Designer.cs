69d64883126f66d733933eda6d75ff66e7a77800
Bostav Flash <bostavflash@gmail.com>
2014-03-18 11:45:17 +0200

    removed files without packages

--- SiteModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Eyad\documents\visual studio 2012\Projects\newSite11-3\newSite11-3\SiteModel.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
